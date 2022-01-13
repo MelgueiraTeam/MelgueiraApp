546a252edc5765a9e506bc5f167cf7f4b2aeb953
elrick046 <87386997+elrick046@users.noreply.github.com>
2022-01-13 16:19:30 -0300

    Correção de Bugs
    Foi resolvido o bug da geração do qr_code

--- lib/ui/criacao_qr_code.dart
+++ lib/ui/criacao_qr_code.dart
@@ -9,6 +9,7 @@ import 'package:qr_flutter/qr_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:gallery_saver/gallery_saver.dart';
 import 'package:share/share.dart';
+import 'package:image/image.dart' as Img;
 
 class QrCodeGenerator extends StatefulWidget {
   int idCaixa;
@@ -26,28 +27,28 @@ class _QrCodeGeneratorState extends State<QrCodeGenerator> {
   Widget build(BuildContext context) {
     final message =
     // ignore: lines_longer_than_80_chars
-        widget.idCaixa.toString();
+    widget.idCaixa.toString();
 
     final qrFutureBuilder = CustomPaint(
       size: Size.square(280.0),
       painter: QrPainter(
-        color: Color(0xff000000),
+        color: Color(0xff123456),
         emptyColor: Color(0xffffffff),
         data: message,
         version: QrVersions.auto,
         eyeStyle: const QrEyeStyle(
           eyeShape: QrEyeShape.square,
-          color: Color(0xff000000),
+          color: Color(0xff123456),
         ),
         dataModuleStyle: const QrDataModuleStyle(
           dataModuleShape: QrDataModuleShape.circle,
-          color: Color(0xff000000),
+          //color: Color(0xff123456),
         ),
         // size: 320.0,
         embeddedImage: null,
         embeddedImageStyle: null,
-        ),
-      );
+      ),
+    );
 
     qr = qrFutureBuilder;
 
@@ -89,8 +90,7 @@ class _QrCodeGeneratorState extends State<QrCodeGenerator> {
     return completer.future;
   }
 
-  _salvar() async{
-
+  _salvar() async {
     String path = await createQrPicture();
 
     await Share.shareFiles(
@@ -103,32 +103,62 @@ class _QrCodeGeneratorState extends State<QrCodeGenerator> {
     final success = await GallerySaver.saveImage(path);
 
     Scaffold.of(context).showSnackBar(SnackBar(
-      content: success ? Text('Image saved to Gallery') : Text('Error saving image'),
+      content: success ? Text('Image saved to Gallery') : Text(
+          'Error saving image'),
     ));
-
   }
 
   Future<String> createQrPicture() async {
-
     Directory tempDir = await getTemporaryDirectory();
     String tempPath = tempDir.path;
-    final ts = DateTime.now().millisecondsSinceEpoch.toString();
+    final ts = DateTime
+        .now()
+        .millisecondsSinceEpoch
+        .toString();
     String path = '$tempPath/$ts.png';
 
     QrPainter qrPainter = qr.painter;
 
 
-    final picData = await qrPainter.toImageData(2048.0, format: ui.ImageByteFormat.png );
+    final picData = await qrPainter.toImageData(
+        2048.0, format: ui.ImageByteFormat.png);
+    //ByteData a;
     await writeToFile(picData, path);
     return path;
   }
 
   Future<void> writeToFile(ByteData data, String path) async {
     final buffer = data.buffer;
-    await File(path).writeAsBytes(
-        buffer.asUint8List(data.offsetInBytes, data.lengthInBytes)
+    var a = buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+    a = await removeBlackBackground(a);
+
+    await File(path).writeAsBytes(a
     );
   }
 
 
+  Future<Uint8List> removeBlackBackground(Uint8List bytes) async {
+    Img.Image image = Img.decodeImage(bytes);
+    Img.Image transparentImage = await colorTransparent(image, 0, 0, 0);
+    var newPng = Img.encodePng(transparentImage);
+    return newPng;
+  }
+
+  Future<Img.Image> colorTransparent(Img.Image src, int red, int green, int blue) async {
+    var pixels = src.getBytes();
+    for (int i = 0, len = pixels.length; i < len; i += 4) {
+      if ((pixels[i] != 18
+        || pixels[i + 1] != 52
+        || pixels[i + 2] != 86)
+      ) {
+        pixels[i + 3] = 255;
+        pixels[i] = 255;
+        pixels[i + 1] = 255;
+        pixels[i + 2] = 255;
+      }
+    }
+
+    return src;
+  }
 }
